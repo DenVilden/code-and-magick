e97ba6816542a0a53f2458d405ec78afc0d130b7
Denis Tolmachev <oovilden@gmail.com>
2018-06-26 20:14:01 +0500

    Получает волшебников с сервера

--- js/setup.js
+++ js/setup.js
@@ -2,49 +2,43 @@
 
 (function () {
 
-  var WIZARD_FIRST_NAME = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  var WIZARD_LAST_NAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-  var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-  var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
-
-  var wizards = [];
-
-  var fragment = document.createDocumentFragment();
   var setup = document.querySelector('.setup');
-  var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-      .content
-      .querySelector('.setup-similar-item');
-
-  // Создает объект волшебника
-  function createWizard() {
-    var wizardProperty = {
-      name: window.random.getRandomItem(WIZARD_FIRST_NAME) + ' ' + window.random.getRandomItem(WIZARD_LAST_NAME),
-      coatColor: window.random.getRandomItem(WIZARD_COAT_COLOR),
-      eyesColor: window.random.getRandomItem(WIZARD_EYES_COLOR)
-    };
-    return wizardProperty;
-  }
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 
   // Рисует волшебника
   function renderWizard(arr) {
     similarWizardTemplate.querySelector('.setup-similar-label').textContent = arr.name;
-    similarWizardTemplate.querySelector('.wizard-coat').style.fill = arr.coatColor;
-    similarWizardTemplate.querySelector('.wizard-eyes').style.fill = arr.eyesColor;
+    similarWizardTemplate.querySelector('.wizard-coat').style.fill = arr.colorCoat;
+    similarWizardTemplate.querySelector('.wizard-eyes').style.fill = arr.colorEyes;
 
     return similarWizardTemplate.cloneNode(true);
   }
 
-  // Собирает волшебников в массив и отрисовывает в DOM
-  function cloneWizard() {
+  // Получает волшебников с сервера
+  function successHandler(wizards) {
+    var fragment = document.createDocumentFragment();
+
     for (var i = 0; i < 4; i++) {
-      wizards.push(createWizard());
-      var clone = renderWizard(wizards[i]);
-      fragment.appendChild(clone);
+      fragment.appendChild(renderWizard(wizards[i]));
     }
     setup.querySelector('.setup-similar-list').appendChild(fragment);
-    // Отображает список похожих персонажей
     setup.querySelector('.setup-similar').classList.remove('hidden');
   }
-  cloneWizard();
+
+  // При ошибке
+  function errorHandler(errorMessage) {
+    var node = document.createElement('div');
+
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  }
+
+  window.load(successHandler, errorHandler);
 
 })();
