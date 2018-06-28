4e8b9095e47a4f8b993b13c756c4bf67a0abadf4
Denis Tolmachev <oovilden@gmail.com>
2018-06-28 22:11:38 +0500

    Объединяет функции отправки запроса

--- js/backend.js
+++ js/backend.js
@@ -2,27 +2,12 @@
 
 (function () {
 
-// Получение запроса
+  // Получение запроса
   window.load = function (onSuccess, onError) {
     var URL = 'https://js.dump.academy/code-and-magick/data';
     var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
 
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onSuccess(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.timeout = 10000; // 10s
+    getResponse(onSuccess, onError, xhr);
 
     xhr.open('GET', URL);
     xhr.send();
@@ -32,6 +17,14 @@
   window.save = function (data, onSuccess, onError) {
     var URL = 'https://js.dump.academy/code-and-magick';
     var xhr = new XMLHttpRequest();
+
+    getResponse(onSuccess, onError, xhr);
+
+    xhr.open('POST', URL);
+    xhr.send(new FormData(data));
+  };
+
+  function getResponse(onSuccess, onError, xhr) {
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
@@ -49,9 +42,6 @@
     });
 
     xhr.timeout = 10000; // 10s
-
-    xhr.open('POST', URL);
-    xhr.send(new FormData(data));
-  };
+  }
 
 })();
