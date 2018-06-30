57b7d8c8ba6a793fcbd3152c2b93549e389eebdb
Denis Tolmachev <oovilden@gmail.com>
2018-07-01 01:55:19 +0500

    Переделывает ajax запрос

--- js/backend.js
+++ js/backend.js
@@ -6,38 +6,14 @@
   window.load = function (onSuccess, onError) {
     var URL = 'https://js.dump.academy/code-and-magick/data';
 
-    getResponse(onSuccess, onError, 'GET', URL);
+    window.ajax.getResponse(onSuccess, onError, 'GET', URL);
   };
 
   // Отправка запроса
   window.save = function (onSuccess, onError, data) {
     var URL = 'https://js.dump.academy/code-and-magick';
 
-    getResponse(onSuccess, onError, 'POST', URL, data);
+    window.ajax.getResponse(onSuccess, onError, 'POST', URL, data);
   };
-
-  function getResponse(onSuccess, onError, method, url, data) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onSuccess(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.timeout = 10000; // 10s
-
-    xhr.open(method, url);
-    xhr.send(new FormData(data));
-  }
 
 })();
