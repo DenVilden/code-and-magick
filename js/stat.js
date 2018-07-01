93003dc2498d55f25921d6f63a36c1c057a0bff2
Denis Tolmachev <oovilden@gmail.com>
2018-07-01 16:14:40 +0500

    Объединяет переменные в объект

--- js/stat.js
+++ js/stat.js
@@ -1,44 +1,52 @@
 'use strict';
 
 (function () {
-  var CLOUD_WIDTH = 420;
-  var CLOUD_HEIGHT = 270;
-  var CLOUD_X = 100;
-  var CLOUD_Y = 10;
-  var COLOR_WHITE = '#fff';
-  var COLOR_GRAY = 'rgba(0, 0, 0, 0.7)';
-  var COLOR_RED = 'rgba(255, 0, 0, 1)';
-  var COLOR_BLACK = '#000';
+  var CLOUD = {
+    width: 420,
+    height: 270,
+    x: 100,
+    y: 10
+  };
+  var COLOR = {
+    white: '#fff',
+    gray: 'rgba(0, 0, 0, 0.7)',
+    red: 'rgba(255, 0, 0, 1)',
+    black: '#000'
+  };
+  var TEXT = {
+    font: '16px PT Mono',
+    height: 90
+  };
+  var BAR = {
+    width: 40,
+    gap: 50,
+    maxHeight: 150
+  };
   var GAP = 10;
-  var TEXT_HEIGHT = 90;
-  var TEXT_FONT = '16px PT Mono';
-  var BAR_WIDTH = 40;
-  var BAR_GAP = 50;
-  var MAX_BAR_HEIGHT = 150;
-  var BAR_MARGIN_LEFT = BAR_WIDTH + BAR_GAP;
-  var CLOUD_PADDING_TOP = MAX_BAR_HEIGHT + TEXT_HEIGHT;
+  var BAR_MARGIN_LEFT = BAR.width + BAR.gap;
+  var CLOUD_PADDING_TOP = BAR.maxHeight + TEXT.height;
 
   // Генерация облако результатов
   function renderCloud(ctx, x, y, color) {
     ctx.fillStyle = color;
-    ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+    ctx.fillRect(x, y, CLOUD.width, CLOUD.height);
   }
 
   // Генерация столбика результатов
   function renderBar(ctx, name, x, y) {
-    ctx.fillStyle = name === 'Вы' ? COLOR_RED : window.random.getRandomColor();
-    ctx.fillRect(CLOUD_X + x, CLOUD_PADDING_TOP + GAP - y, BAR_WIDTH, y);
+    ctx.fillStyle = name === 'Вы' ? COLOR.red : window.random.getRandomColor();
+    ctx.fillRect(CLOUD.x + x, CLOUD_PADDING_TOP + GAP - y, BAR.width, y);
   }
 
   // Генерация времени в секундах
   function renderTime(ctx, time, x, y) {
-    ctx.fillText(Math.round(time), CLOUD_X + x, CLOUD_PADDING_TOP - GAP - y);
+    ctx.fillText(Math.round(time), CLOUD.x + x, CLOUD_PADDING_TOP - GAP - y);
   }
 
   // Генерация имен игроков
   function renderName(ctx, name, x) {
-    ctx.fillStyle = COLOR_BLACK;
-    ctx.fillText(name, CLOUD_X + x, CLOUD_Y + CLOUD_HEIGHT - GAP * 2);
+    ctx.fillStyle = COLOR.black;
+    ctx.fillText(name, CLOUD.x + x, CLOUD.y + CLOUD.height - GAP * 2);
   }
 
   // Генерация текста список результатов
@@ -46,26 +54,26 @@
     ctx.font = font;
     ctx.fillStyle = color;
     ctx.textBaseline = 'hanging';
-    ctx.fillText('Ура вы победили!', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 2);
-    ctx.fillText('Список результатов:', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 4);
+    ctx.fillText('Ура вы победили!', CLOUD.x + GAP * 2, CLOUD.y + GAP * 2);
+    ctx.fillText('Список результатов:', CLOUD.x + GAP * 2, CLOUD.y + GAP * 4);
   }
 
   window.renderStatistics = function (ctx, names, times) {
     var maxTime = window.random.getMaxElement(times);
 
     // Отрисовывает тень облака
-    renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, COLOR_GRAY);
+    renderCloud(ctx, CLOUD.x + GAP, CLOUD.y + GAP, COLOR.gray);
 
     // Отрисовывает облако
-    renderCloud(ctx, CLOUD_X, CLOUD_Y, COLOR_WHITE);
+    renderCloud(ctx, CLOUD.x, CLOUD.y, COLOR.white);
 
     // Отрисовывает текст список результатов
-    renderText(ctx, TEXT_FONT, COLOR_BLACK);
+    renderText(ctx, TEXT.font, COLOR.black);
 
     // Гистограмма
     for (var i = 0; i < names.length; i++) {
-      var barWidth = BAR_GAP + BAR_MARGIN_LEFT * i;
-      var barHeight = (MAX_BAR_HEIGHT * times[i]) / maxTime;
+      var barWidth = BAR.gap + BAR_MARGIN_LEFT * i;
+      var barHeight = (BAR.maxHeight * times[i]) / maxTime;
 
       // Время в секундах
       renderTime(ctx, times[i], barWidth, barHeight);
